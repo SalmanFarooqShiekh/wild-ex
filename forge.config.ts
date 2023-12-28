9aca6a99d82e5b27bb14c99c2287907f5a6a6653
Salman Farooq <salman.farooq3310@gmail.com>
2023-12-28 22:37:58 +0500

    .

--- forge.config.ts
+++ forge.config.ts
@@ -3,12 +3,12 @@ import { MakerSquirrel } from "@electron-forge/maker-squirrel";
 import MakerDMG from "@electron-forge/maker-dmg";
 import { MakerDeb } from "@electron-forge/maker-deb";
 import { MakerRpm } from "@electron-forge/maker-rpm";
+import MakerZIP from "@electron-forge/maker-zip";
 import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-natives";
 import { WebpackPlugin } from "@electron-forge/plugin-webpack";
-
 import { mainConfig } from "./webpack.main.config";
-import { rendererConfig } from "./webpack.renderer.config";
 
+import { rendererConfig } from "./webpack.renderer.config";
 import { join } from "path";
 
 const icon_path = join(__dirname, "/src/assets/icon.png");
@@ -22,11 +22,15 @@ const config: ForgeConfig = {
   makers: [
     new MakerSquirrel({}),
     new MakerDMG({ name: "WildEx", icon: icon_path, overwrite: true }),
-    new MakerRpm({}),
-    new MakerDeb({}),
+    // new MakerRpm({}),
+    // new MakerDeb({}),
+    new MakerZIP({}),
   ],
   plugins: [
-    new AutoUnpackNativesPlugin({}),
+    {
+      name: "@electron-forge/plugin-auto-unpack-natives",
+      config: {},
+    },
     new WebpackPlugin({
       mainConfig,
       renderer: {
