939b9b6a5d58fc2f149f5160816f37f00963e329
Salman Farooq <salman.farooq3310@gmail.com>
2023-12-28 20:53:22 +0500

    .

--- webpack.main.config.ts
+++ webpack.main.config.ts
@@ -21,4 +21,7 @@ export const mainConfig: Configuration = {
   resolve: {
     extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".json"],
   },
+  externals: {
+    'sharp': 'commonjs sharp'
+  },
 };
